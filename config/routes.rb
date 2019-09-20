1cb29dc61d527dce42a37935d8a40c8120bb2359
Emily Lillie <emilylillie3@gmail.com>
2019-09-20 21:18:47 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'students', to: 'student#students'
+  get 'students', to: 'student#index'
   # Create a route that is mapped to a students' controller index action, the resulting route should be /students
 end
