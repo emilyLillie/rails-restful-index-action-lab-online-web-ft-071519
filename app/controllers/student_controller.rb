99925cbda9855c9fb9b81ae7ed2d5a001136f9e8
Emily Lillie <emilylillie3@gmail.com>
2019-09-20 21:11:58 +0000

    Automatically backed up by Learn

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -4,3 +4,7 @@ class StudentController < ApplicationController
     render "index"
   end
 end 
+
+# Create a controller for students and have it inherit from ApplicationController
+
+# Integrate a controller action in the students' controller for handling the index action
